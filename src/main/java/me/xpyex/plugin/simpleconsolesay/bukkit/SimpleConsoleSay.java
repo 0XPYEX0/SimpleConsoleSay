e382d2a54749bb0f5e245e00768d1d71e847fff9
XPYEX <XPYEX0@163.com>
2024-05-14 01:15:53 +0800

    变更

--- src/main/java/me/xpyex/plugin/simpleconsolesay/bukkit/SimpleConsoleSay.java
+++ src/main/java/me/xpyex/plugin/simpleconsolesay/bukkit/SimpleConsoleSay.java
@@ -1,6 +1,8 @@
 package me.xpyex.plugin.simpleconsolesay.bukkit;
 
 import java.lang.reflect.Field;
+import org.bukkit.command.CommandException;
+import org.bukkit.command.CommandSender;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -17,7 +19,7 @@ public final class SimpleConsoleSay extends JavaPlugin {
         getLogger().info("启动中");
         CLASS_LOADER = getClassLoader();
         getServer().getPluginManager().registerEvents(new Listener() {
-            @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+            @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
             public void onServerCmd(ServerCommandEvent event) {
                 if (HANDLING) {
                     return;
@@ -25,15 +27,14 @@ public final class SimpleConsoleSay extends JavaPlugin {
                 HANDLING = true;
                 if (event.getCommand().startsWith("/")) {
                     event.setCancelled(true);
-                    getServer().dispatchCommand(event.getSender(), event.getCommand().substring(1));
+                    executeCmdSafely(event.getSender(), event.getCommand().substring(1));
                 } else if (trace() == null) {  //如果trace()返回非空，那就是插件处理的
                     event.setCancelled(true);
-                    getServer().dispatchCommand(event.getSender(), "say " + event.getCommand());
+                    executeCmdSafely(event.getSender(), "say " + event.getCommand());
                 }
                 HANDLING = false;
             }
         }, this);
-        // Plugin startup logic
     }
 
     @Override
@@ -42,28 +43,39 @@ public final class SimpleConsoleSay extends JavaPlugin {
         // Plugin shutdown logic
     }
 
+    private void executeCmdSafely(CommandSender sender, String cmd) {
+        try {
+            getServer().dispatchCommand(sender, cmd);
+        } catch (Throwable e) {
+            getLogger().info("出现错误: " + e);
+            e.printStackTrace();
+        }
+    }
+
     public static Plugin trace() {
-        StackTraceElement[] stackTrace = new Throwable().getStackTrace();
-        // 从最前面找 最初的调用者
-        for (int i = stackTrace.length - 1; i >= 0; i--) {
-            try {
-                ClassLoader classLoader = Class.forName(stackTrace[i].getClassName()).getClassLoader();
-                // 不能找到自己
-                if (classLoader == null || classLoader.equals(CLASS_LOADER)) {
-                    continue;
+        try {
+            StackTraceElement[] stackTrace = new Throwable().getStackTrace();
+            // 从最前面找 最初的调用者
+            for (int i = stackTrace.length - 1; i >= 0; i--) {
+                try {
+                    ClassLoader classLoader = Class.forName(stackTrace[i].getClassName()).getClassLoader();
+                    // 不能找到自己
+                    if (classLoader == null || classLoader.equals(CLASS_LOADER)) {
+                        continue;
+                    }
+
+                    // 找这个 plugin 字段
+                    Field pluginField = classLoader.getClass().getDeclaredField("plugin");
+                    pluginField.setAccessible(true);
+                    return (Plugin) pluginField.get(classLoader);
+                } catch (ClassNotFoundException | IllegalAccessException e) {
+                    throw new IllegalStateException(e);
+                } catch (NoSuchFieldException | ClassCastException e) {
+                    // 找不到plugin字段说明 不是 PluginClassLoader 类
                 }
 
-                // 找这个 plugin 字段
-                Field pluginField = classLoader.getClass().getDeclaredField("plugin");
-                pluginField.setAccessible(true);
-                return (Plugin) pluginField.get(classLoader);
-            } catch (ClassNotFoundException | IllegalAccessException e) {
-                throw new IllegalStateException(e);
-            } catch (NoSuchFieldException | ClassCastException e) {
-                // 找不到plugin字段说明 不是 PluginClassLoader 类
             }
-
-        }
+        } catch (Throwable ignored) {}
         return null;
     }
 }
